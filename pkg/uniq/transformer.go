ede9607e93357771e6925579f3ae6890b0238dea
Leontyev Andrey <las24u327@student.bmstu.ru>
2025-11-05 13:21:59 +0300

    bashnya_hw4 transformers fixed

--- pkg/uniq/transformer.go
+++ pkg/uniq/transformer.go
@@ -1,37 +1,55 @@
 package uniq
 
-import "strings"
+import (
+    "strings"
+    "unicode"
+)
 
 func SkipFields(line string, numFields int) string {
-	if numFields <= 0 {
-		return line
-	}
+    if numFields == 0 {
+        return line
+    }
 
-	fields := strings.Fields(line)
+    fieldCount := 0
+    inField := false
+    
+    for i, r := range line {
+        isSpace := unicode.IsSpace(r)
+        
+        if !isSpace && !inField {
+            fieldCount++
+            inField = true
+            
+            if fieldCount > numFields {
+                return line[i:]
+            }
+        }
+        
+        if isSpace && inField {
+            inField = false
+        }
+    }
 
-	if numFields >= len(fields) {
-		return ""
-	}
-
-	return strings.Join(fields[numFields:], " ")
+    return ""
 }
 
 func SkipChars(line string, numChars int) string {
-	if numChars <= 0 {
-		return line
-	}
-
-	if numChars >= len(line) {
-		return ""
-	}
-
-	return string(line[numChars:])
+    if numChars == 0 {
+        return line
+    }
+    
+    runes := []rune(line)
+    
+    if len(runes) <= numChars {
+        return ""
+    }
+    
+    return string(runes[numChars:])
 }
 
 func NormalizeCase(line string, ignoreCase bool) string {
-	if !ignoreCase {
-		return line
-	}
-
-	return strings.ToLower(line)
+    if ignoreCase {
+        return strings.ToLower(line)
+    }
+    return line
 }
